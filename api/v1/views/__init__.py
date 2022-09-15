031757200ebfe42bf34d962d58e33b6492b5b8c7
Alejo Aragon <alejo.aragon.05@gmail.com>
2022-09-15 14:02:24 -0500

    update_project

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Init file for flask views"""
+
+from flask import Blueprint
+
+app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1')
---
+++ api/v1/views/index.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Index file for Flask blueprints"""
+
+
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', strict_slashes=False)
+def ret_status():
+    return(jsonify(status="OK"))
