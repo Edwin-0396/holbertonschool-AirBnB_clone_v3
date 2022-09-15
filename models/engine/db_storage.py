5912c1bb811a1fe5070cf521f4dfcd9922241c54
Alejo Aragon <alejo.aragon.05@gmail.com>
2022-09-15 12:39:58 -0500

    update_project

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,16 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Retrieve an object"""
+        cls_dict = self.all(cls)
+        for obj in cls_dict.values():
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """Count number of objects in storage"""
+        cls_dict = self.all(cls)
+        return len(cls_dict.values())
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,16 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+            """Retrieve an object"""
+            cls_dict = self.all(cls)
+            for obj in cls_dict.values():
+                if obj.id == id:
+                    return obj
+            return None
+
+    def count(self, cls=None):
+        """Count number of objects in storage"""
+        cls_dict = self.all(cls)
+        return len(cls_dict.values())
