4a86d4dc5474228b32c6af1106cc4142760e6bdd
tluciad <tluciad@gmail.com>
2022-09-15 19:04:28 -0500

    status

--- api/v1/app.py
+++ api/v1/app.py
@@ -9,9 +9,11 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_close(self):
     storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST')
