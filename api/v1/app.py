2f5c3379a5a86663ca7f7c41f24fb14d20b49b6e
Edwin-9603 <egc4611@gmail.com>
2022-09-15 20:46:08 -0400

    new updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from api.v1.views import app_views
 from flask import Flask
 from models import storage
 import os
+from flask import jsonify, make_response
 
 
 app = Flask(__name__)
@@ -13,6 +14,10 @@ app.register_blueprint(app_views)
 @app.teardown_appcontext
 def teardown_appcontext(self):
     storage.close()
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
