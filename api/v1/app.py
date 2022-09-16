96bf96c13f55e4e14d7c5e1d40f3095d0e813442
Alejo Aragon <alejo.aragon.05@gmail.com>
2022-09-15 19:08:26 -0500

    update_project

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """First blueprint for HBNB Project"""
-
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
 import os
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def teardown_close(self):
