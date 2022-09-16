99f4a0a17befb4a58d4a611cf71e74fa9128513a
Alejo Aragon <alejo.aragon.05@gmail.com>
2022-09-15 22:17:29 -0500

    update_project cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -14,14 +14,17 @@ def get_cities(state_id):
     state = storage.get("State", state_id)
     if not state:
         abort(404)
-    return jsonify([city.to_dict() for city in state.cities])
+    cities = []
+    for city in state.cities:
+        cities.append(city.to_dict())
+    return jsonify(cities)
 
 
 @app_views.route('cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city_id(city_id):
     """get information for city"""
     city = storage.get("City", city_id)
-    if not city:
+    if city is None:
         abort(404)
     return jsonify(city.to_dict())
 
@@ -29,7 +32,7 @@ def get_city_id(city_id):
 @app_views.route('cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     city = storage.get("City", city_id)
-    if not city:
+    if city is None:
         abort(404)
     city.delete()
     storage.save()
@@ -40,16 +43,16 @@ def delete_city(city_id):
 def create_city(state_id):
     """Creates a City"""
     state = storage.get("State", state_id)
-    if not state:
+    if state is None:
         abort(404)
-    city_new = request.get_json()
-    if not city_new:
-        abort(400, 'Not a JSON')
-    if 'name' not in city_new:
-        abort(400, 'Missing name')
-    city = City(**city_new)
-    storage.new(city)
-    storage.save()
+    if not request.get_json():
+        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+    if 'name' not in request.get_json():
+        return make_response(jsonify({'error': 'Missing name'}), 400)
+    kwargs = request.get_json()
+    kwargs['state_id'] = state_id
+    city = City(**kwargs)
+    city.save()
     return make_response(jsonify(city.to_dict()), 201)
 
 
